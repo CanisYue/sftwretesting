aa8030ab1b33edeb0587425319a3613e9b431fee
canisyue <1025221390@qq.com>
2022-09-30 09:29:45 -0500

    init commit

--- demo.sh
+++ demo.sh
@@ -37,7 +37,7 @@ readonly JDK_DIR="${DL_DIR}/jdk-11.0.8+10"
 if [[ ! -f ${JDK_DIR}/bin/java || ! -f ${JDK_DIR}/bin/javac ]]; then
         echo "Download JDK..."
         rm -f "${DL_DIR}/openjdk11.tar.gz"
-        wget -q 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz' -O ${DL_DIR}/openjdk11.tar.gz
+        wget -q 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jdk_aarch64_linux_hotspot_11.0.9.1_1.tar.gz' -O ${DL_DIR}/openjdk11.tar.gz
         if [[ $? -ne 0 ]]; then
                 echo "ERROR: Downloading JDK failed!" >&2
                 exit 1
